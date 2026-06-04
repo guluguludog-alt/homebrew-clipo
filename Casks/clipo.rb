598a4388c7b6f1ef9a68eca63ff8673f9e9cb6b8
normanliu <1413591899@qq.com>
2026-06-05 00:16:37 +0800

    homebrew

--- Casks/clipo.rb
+++ Casks/clipo.rb
@@ -1,6 +1,6 @@
 cask "clipo" do
-  version "1.1.5"
-  sha256 "ab6b226355c52f775ca3b01d2faf7b97ebeeeac283308e3cddf67eb32a3d9215"
+  version "1.1.6"
+  sha256 "b368e5deaa2a0170811307d992cfe4859be9c80609502e49bda823dd5eb2e384"
 
   url "https://github.com/guluguludog-alt/clipo-release/releases/download/v#{version}/Clipo_Homebrew.dmg"
   name "Clipo"
