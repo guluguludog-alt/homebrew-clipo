54f58516a2b4a5a477726e4c60bd0e0c562d5cca
normanliu <1413591899@qq.com>
2026-06-07 22:25:24 +0800

    Update Clipo Homebrew cask URL and checksum

--- Casks/clipo.rb
+++ Casks/clipo.rb
@@ -1,8 +1,8 @@
 cask "clipo" do
   version "1.2.0"
-  sha256 "ec6ea34880db1e415ca62a7a221388cc629972ab584df9c38c08331d55319572"
+  sha256 "3e4ef3128c32f01dfdbfc15f20b6db66f5a650df7300ae43b066f1d4aa57d688"
 
-  url "https://github.com/guluguludog-alt/clipo-release/releases/download/v#{version}/Clipo_Homebrew.dmg"
+  url "https://github.com/guluguludog-alt/clipo/releases/download/v#{version}/Clipo_Homebrew.dmg"
   name "Clipo"
   desc "剪贴板管理工具"
   homepage "https://github.com/guluguludog-alt/Clipo"
