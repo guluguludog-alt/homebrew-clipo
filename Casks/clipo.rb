2c5a49f6563b7c8637207594eef2f4e4221285ba
normanliu <1413591899@qq.com>
2026-06-22 19:42:57 +0800

    更新 SHA256: 0506d38c5841aed2d4720d3f63298047d28689310825f7f69c4ab3ef810a3611

--- Casks/clipo.rb
+++ Casks/clipo.rb
@@ -1,6 +1,6 @@
 cask "clipo" do
   version "1.2.2"
-  sha256 "2cd0e2e04054e6dbef7bfd5fdb3985f6e4be1c14a1e3c6aca3700fd890d77c5c"
+  sha256 "0506d38c5841aed2d4720d3f63298047d28689310825f7f69c4ab3ef810a3611"
 
   url "https://github.com/guluguludog-alt/clipo/releases/download/v#{version}/Clipo_Homebrew.dmg"
   name "Clipo"
