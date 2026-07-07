0721cfb9c1373e43b096451653c966ab1a7e1649
normanliu <1413591899@qq.com>
2026-07-07 23:50:03 +0800

    Update clipo cask to v1.2.6

--- Casks/clipo.rb
+++ Casks/clipo.rb
@@ -1,6 +1,6 @@
 cask "clipo" do
-  version "1.2.5"
-  sha256 "c1948a48d3470e4657f4c1d936052dcf2aaf9405c1b7bc4e4dfa87eb44e0ed35"
+  version "1.2.6"
+  sha256 "28141fc2b5205483b2fdb4eb45ba0473d030d4071b10681b9c91b9448acc522a"
 
   url "https://github.com/guluguludog-alt/clipo/releases/download/v#{version}/Clipo_Homebrew.dmg"
   name "Clipo"
