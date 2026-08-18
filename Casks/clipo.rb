915c726fa0e5c7106fb36d9bfe3735db481af660
normanliu <1413591899@qq.com>
2026-08-19 00:26:30 +0800

    Update Clipo cask to v1.2.9.1

--- Casks/clipo.rb
+++ Casks/clipo.rb
@@ -1,11 +1,13 @@
 cask "clipo" do
-  version "1.2.9"
-  sha256 "26911780bee448a10dfc4f3a5eac09b51ad194b84d16f8021ab7f5b510568256"
+  version "1.2.9.1"
+  sha256 "d461b02d6ff1cbc2a7932607a06b78b2f232041cade52082baef64a232e2c203"
 
   url "https://github.com/guluguludog-alt/clipo/releases/download/v#{version}/Clipo_Homebrew.dmg"
   name "Clipo"
   desc "剪贴板管理工具"
   homepage "https://github.com/guluguludog-alt/Clipo"
+
+  depends_on :macos
 
   app "Clipo.app"
 
